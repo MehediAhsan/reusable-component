55c5b4594f7221527e5c7d0305a067b6c3fdb26d
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-11-13 23:49:05 +0600

    added navbar

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,44 +1,60 @@
+import {FaBars, FaTimes} from 'react-icons/fa'
 import { useState } from "react";
+import { FaHome, FaEnvelope, FaUtensils, FaShoppingCart, FaTools } from "react-icons/fa";
 
 const TopNavbar = () => {
-  const [navigate, setNavigate] = useState(false);
 
+    const Menus = [
+        {
+            name: 'Home', 
+            link:'/',
+            icon: <FaHome></FaHome>
+        },
+        {
+            name: 'CONTACT US', 
+            link:'/',
+            icon: <FaEnvelope></FaEnvelope>
+        },
+        {
+            name: 'Our Menu', 
+            link:'/',
+            icon: <FaUtensils></FaUtensils>
+        },
+        {
+            name: 'Our Shop', 
+            link:'/',
+            icon: <FaShoppingCart></FaShoppingCart>
+        },
+
+    ]
+
+    let [open, setOpen] = useState(false)
 
     return (
-<nav className="bg-white border-gray-200 dark:bg-gray-900 relative z-40">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-6 pl-16 sm:pl-4">
-    <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MyLogo</span>
-    </a>
-    <button onClick={() => setNavigate(!navigate)} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-    <div className={`${navigate ? "block" : "hidden"} md:block w-full md:w-auto`} id="navbar-default">
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-          <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
+        <div className="bg-white w-full top-0 left-0 z-50 ">
+            <div className="flex items-center justify-between px-14 py-7">
+                <div className="flex justify-center items-center text-2xl font-semibold gap-3">
+                    <FaTools className="text-blue-500 text-3xl"></FaTools>
+                    <a to="/"><h1 className="font-semibold font-sans italic">Swader Songe</h1></a>
+                </div>
+                <div>
+                    <div onClick={() => setOpen(!open)} className="md:hidden absolute top-8 right-10 text-xl">
+                        {
+                            open ? <FaTimes></FaTimes> : <FaBars></FaBars> 
+                        }
+                    </div>
+                    
+                    <ul className={`md:flex md:items-center justify-center absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto pl-9 md:pl-0 py-4 md:py-0 uppercase ${open ? 'top-20 opacity-100': 'top-[-490px] md:opacity-100 opacity-0'} `}>
+                        {
+                            Menus.map(Menu => <li key={Menu.name} className="flex justify-center items-center gap-1 md:ml-6 my-6 md:my-0 transition duration-300 hover:text-blue-500 font-medium">
+                                <span className="text-xl text-blue-500">{Menu.icon}</span>
+                               <a to={Menu.link}>{Menu.name}</a> 
+                            </li>)
+                        }
+                    </ul>
+                </div>
+            </div>
+        </div>
     );
 };
 
